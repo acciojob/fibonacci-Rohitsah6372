891662f9b94662b136fcf33d58e41383a2406c85
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-09-07 10:51:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function fibonacci(num) {
-	if(num == 0 || num ==1)
-		return nums;
+    if (num === 0) {
+        return 0; // Fibonacci(0) is 0
+    } else if (num === 1) {
+        return 1; // Fibonacci(1) is 1
+    }
 
-
-	return fibonacci(num-1) + fibonacci(num -2);
-
+    // Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
